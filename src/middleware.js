a895e8c3abe08b919ad21452291a1820fed21340
Ksperix <kacper.chlanda@gmail.com>
2026-09-07 19:20:19 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,7 @@
-import createMiddleware from 'next-intl/middleware';
-
-export default createMiddleware({
-  locales: ['pl', 'en'],
-  defaultLocale: 'pl',
-  localePrefix: 'as-needed' // <--- Nie dodaje /pl do głównego adresu
-});
-
-export const config = {
-  // Wykluczamy pliki statyczne, favicony i api, by middleware ich nie blokował
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)']
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
