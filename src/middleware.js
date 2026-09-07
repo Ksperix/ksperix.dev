0b957752365ec4bb62f7385174c34376b3e67521
Ksperix <kacper.chlanda@gmail.com>
2026-09-07 19:19:03 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -2,9 +2,11 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   locales: ['pl', 'en'],
-  defaultLocale: 'pl'
+  defaultLocale: 'pl',
+  localePrefix: 'as-needed' // <--- Nie dodaje /pl do głównego adresu
 });
 
 export const config = {
-  matcher: ['/', '/(pl|en)/:path*']
+  // Wykluczamy pliki statyczne, favicony i api, by middleware ich nie blokował
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)']
 };
